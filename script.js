a72b9653d34e9a6771a471a60f76dba540f7c6a2
tohidshabanloo <tshabanloo@hotmail.com>
2022-03-03 17:56:31 +0330

    complete 2

--- script.js
+++ script.js
@@ -29,6 +29,24 @@ form.addEventListener("submit", (e) => {
     task_input_el.setAttribute("readonly", "readonly");
 
     task_content_el.appendChild(task_input_el);
+
+    const task_actions_el = document.createElement("div");
+    task_actions_el.classList.add("actions");
+
+    const task_edit_el = document.createElement("button");
+    task_edit_el.innerHTML = "Edit";
+    task_edit_el.classList.add("edit");
+
+    const task_delete_el = document.createElement("button");
+    task_delete_el.innerHTML = "Delete";
+    task_delete_el.classList.add("delete");
+
+    task_actions_el.appendChild(task_edit_el);
+    task_actions_el.appendChild(task_delete_el);
+
+    task_el.appendChild(task_actions_el);
+
+    list_el.appendChild(task_el);
 }
 
 
